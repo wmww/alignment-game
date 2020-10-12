ca40cc6a9abf4fccdb501d130c92d90e05c827ac
William Wold <wm@wmww.sh>
2020-10-12 08:04:54 -0700

    Implement tweeting with the API

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import twitter_api
+
 from dotenv import load_dotenv
-load_dotenv(verbose=True)
+import logging
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    load_dotenv(verbose=True)
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+    logger.info('Posting Tweet…')
+    twitter_api.post_text_tweet('Hello Twitter')
---
+++ twitter_api.py
@@ -0,0 +1,40 @@
+from twitter import Api
+from dotenv import load_dotenv
+import os
+from os import environ
+import logging
+
+secrets_path = 'secrets/secrets.env'
+
+logger = logging.getLogger(__name__)
+api_global = None
+
+def get_environ_or_raise(name: str):
+    value = environ.get(name)
+    if value:
+        return value
+    else:
+        raise RuntimeError('environment variable ' + name + ' not set')
+
+def lazy_get_api():
+    global api_global
+    if api_global:
+        return api_global
+    if not os.path.exists(secrets_path):
+        logger.warning(secrets_path + ' does not exists, secrets will need to be stored in environment variables')
+    load_dotenv(secrets_path)
+    consumer_key = get_environ_or_raise('TWITTER_CONSUMER_API_KEY')
+    consumer_secret = get_environ_or_raise('TWITTER_CONSUMER_API_SECRET_KEY')
+    access_token_key = get_environ_or_raise('TWITTER_ACCESS_TOKEN')
+    access_token_secret = get_environ_or_raise('TWITTER_ACCESS_TOKEN_SECRET')
+    logger.info('Building new Twitter API object')
+    api_global = Api(consumer_key=consumer_key,
+        consumer_secret=consumer_secret,
+        access_token_key=access_token_key,
+        access_token_secret=access_token_secret)
+    return api_global
+
+def post_text_tweet(message: str):
+    api = lazy_get_api()
+    status = api.PostUpdate(message)
+    logger.info('Posted "' + str(status.text) + '" on behalf of @' + str(status.user.screen_name))
